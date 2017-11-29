b8496b2f621531d4d0385db67d4890bcc28a903f
FilipSoft <32578313+FilipSoft@users.noreply.github.com>
2017-11-29 17:55:02 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
   <meta charset="utf-8"/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <link rel="stylesheet" href="home.css">
-    <link rel="shortcut icon" href="assets/home.ico" />
     <title><?php $_GET['page']?></title>
 </head> 
 <body>
