b22fd793a291e701984c9bbfdca04873bffaf52d
uw-adam <lookup@virusinternals.com>
2017-12-08 15:52:29 +0100

    -Created display.php -> displays the articles

--- display.php
+++ display.php
@@ -20,9 +20,9 @@ require("Db.php");
      *  6: added - varchar(60)
      *  7: published - boolean
      */
-    Db::connect("localhost", "blog-kluci", "bloguser", "blogpass");
+    Db::connect("localhost", "blog_kluci", "bloguser", "blogpass");
     $articles = Db::queryAll("SELECT * FROM articles WHERE published=true");
-    die(print_r($articles));
+    
 
 ?>
 
@@ -33,9 +33,9 @@ require("Db.php");
 </head>
 <body>
 
-    <div class="row">
-
-    </div>
+    <?php foreach($articles as $article) : ?>
+	<p>Jmeno: <?= $article["name"] ?> </p>
+    <?php endforeach ?>	
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
